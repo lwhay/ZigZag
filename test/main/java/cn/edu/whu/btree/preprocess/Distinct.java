0b9cea1c20c694f76f6b21b1661838707e349740
lwhay <lwh@whu.edu.cn>
2019-01-06 17:18:06 +0800

    Add Baseline

--- test/main/java/cn/edu/whu/btree/preprocess/Distinct.java
+++ test/main/java/cn/edu/whu/btree/preprocess/Distinct.java
@@ -1,10 +1,23 @@
 package cn.edu.whu.btree.preprocess;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Distinct {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
+        String line = "";
+        int lc = 0;
+        int wc = 0;
+        while ((line = br.readLine()) != null) {
+            String[] words = line.split(" ");
+            wc += words.length;
+            lc++;
+        }
+        br.close();
+        System.out.println(lc + "\t" + wc);
     }
 
 }
