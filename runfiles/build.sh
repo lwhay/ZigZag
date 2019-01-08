1c07ac710fde0f6cba7686ef4aa33c76d8ef43fc
lwhay <lwh@whu.edu.cn>
2019-01-08 14:30:40 +0800

    Revise minor issues and print all records.

--- runfiles/build.sh
+++ runfiles/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx280g -jar TfidfPrefIndexBuild.jar 64000000 mt.txt tokenBtree 27000 4096 2 0 60000 4096 1 0 15000 4000 4096 1 0 5000 4096 2 0 6000 4096 2 0 0 500
+java -Xmx280g -jar TfidfPrefIndexBuild.jar 64000000 mt.txt ./index 27000 4096 2 0 60000 4096 1 0 15000 4000 4096 1 0 5000 4096 2 0 6000 4096 2 0 0 500
--- runfiles/test-hdd-ssd.sh
+++ runfiles/test-hdd-ssd.sh
@@ -2,15 +2,15 @@
 for t in 0.5 0.6 0.7 0.8 0.9
 do
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoBaselineTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoBaselineTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoZigTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoZigTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoZigPlusTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoZigPlusTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoZagTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoZagTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoZagPlusTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoZagPlusTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 	echo 3 > /proc/sys/vm/drop_caches
-	java -Xmx1g -jar AlgoZigZagTest.jar ./tokenBtree ./query-stanford.txt $t 0 0 1 0
+	java -Xmx1g -jar AlgoZigZagTest.jar ./index ./query-stanford.txt $t 0 0 1 0
 done
